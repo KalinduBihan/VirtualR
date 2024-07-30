66795068af14b35eae44e3e381839879042b097f
Kalindu Bihan Fernando <99331732+KalinduBihan@users.noreply.github.com>
2024-07-30 11:13:01 +0530

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Pricing from "./components/Pricing";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -10,6 +14,10 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonial />
+        <Footer />
       </div>
     </>
   );
