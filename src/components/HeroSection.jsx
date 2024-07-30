4e89ecdd2dfbd78177520a211e9298b61c44d9cd
Kalindu Bihan Fernando <99331732+KalinduBihan@users.noreply.github.com>
2024-07-30 09:24:49 +0530

    Initial commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Video1 from "../assets/video1.mp4";
+import Video2 from "../assets/video2.mp4";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center my-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        VirtualR build tools{" "}
+        <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+          for developers
+        </span>
+      </h1>
+      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality!
+      </p>
+      <div className="flex justify-center my-10">
+        <a
+          href="#"
+          className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
+        >
+          Start for free
+        </a>
+        <a href="#" className="py-3 px-4 mx-3 rounded-md border">
+          {" "}
+          Documentation
+        </a>
+      </div>
+      <div className="flex mt-10 justify-center">
+        <video
+          autoPlay
+          muted
+          loop
+          className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"
+        >
+          <source src={Video1} type="video/mp4" />
+          Your browser does not support the video format.
+        </video>
+        <video
+          autoPlay
+          muted
+          loop
+          className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"
+        >
+          <source src={Video2} type="video/mp4" />
+          Your browser does not support the video format.
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
